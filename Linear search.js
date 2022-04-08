f11d52790daaff55632db51a693382c92fe5920f
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-04-08 22:38:30 +0700

    add Linear search

---
+++ Linear search.js
@@ -0,0 +1,9 @@
+const linearSearch = (arr, item) => {
+    for (const i in arr) {
+        if (arr[i] === item) return +i;
+    }
+    return -1;
+};
+
+linearSearch([2, 9, 9], 9); // 1
+linearSearch([2, 9, 9], 7); // -1
